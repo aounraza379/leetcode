cff90846fa3d2f5cbc538e7306ad20a36329e8ea
aounraza379 <aounmohsin1009@gmail.com>
2026-08-13 05:04:41 +0500

    length of last word updated solution

--- top_interview_150/length_of_last_word.py
+++ top_interview_150/length_of_last_word.py
@@ -1,5 +1,17 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        word = s.split()
-        count = len(word[-1])
+        count = 0
+        for i in range(len(s) -1, -1, -1):
+            if s[i] == ' ':
+                if count>0:
+                    return count
+            else:
+                count = count+1
         return count
+
+# WITH BUILT-IN FUNCTION
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         word = s.split()
+#         count = len(word[-1])
+#         return count
