d1ce6d0887036e8e82599392adf522df44c2a72f
aounraza379 <aounmohsin1009@gmail.com>
2026-08-14 19:16:08 +0500

    majority element improved time complexity

--- top_interview_150/majority_element.py
+++ top_interview_150/majority_element.py
@@ -1,5 +1,28 @@
+# 1)
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        length = len(nums)
-        return(nums[length//2])
+        count = 0
+        current_element = None
+
+        for num in nums:
+            if count == 0:
+                current_element = num
+            
+            if num == current_element:
+                count += 1
+            else:
+                count -= 1
+        return current_element 
+        
+# Time: O(n)
+# Extra space: O(1)
+
+# 2)
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         nums.sort()
+#         length = len(nums)
+#         return(nums[length//2])
+
+# Time: O(n log n)
+# Extra space: O(1)
